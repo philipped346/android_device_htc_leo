5a2b56d8a491bcf89eefb51bd56c9b3d0a6869d8
walter79 <gzwalter79@gmail.com>
2014-03-12 20:58:12 +0100

    update for AOKP kitkat ION

--- cm.mk
+++ cm.mk
@@ -2,20 +2,17 @@
 TARGET_USE_KEYBOARD := international
 
 # Call this first so apn list is actually copied
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 # Inherit AOSP device configuration for leo.
 $(call inherit-product, device/htc/leo/device_leo.mk)
 
 # Inherit some common evervolv stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# $(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_MODEL := HTC HD2
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BRAND := htc
 PRODUCT_DEVICE := leo
-PRODUCT_NAME := cm_leo
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=leo BUILD_FINGERPRINT=google/htc_leo/leo:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="leo-user 4.2.2 JDQ39 573038 release-keys"
+PRODUCT_NAME := aokp_leo
