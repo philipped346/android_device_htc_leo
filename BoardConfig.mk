5a12d7d621f98a3be495f15556e0f55a49efbc61
walter79 <gzwalter79@gmail.com>
2014-03-13 12:44:27 +0100

    use default toolchain , 2.6.32 or 2.6.35 kernel...
    .... needs 4.4.3 toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,9 +58,7 @@
   BOARD_KERNEL_CMDLINE := no_console_suspend=1 wire.search_count=5
   BOARD_KERNEL_BASE := 0x11800000
   BOARD_KERNEL_NEW_PPPOX := true
-  # only for carbon rom
-  TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.4.3/bin/arm-eabi-
-
+  
 
 # bootanimation
   TARGET_BOOTANIMATION_HALF_RES := true
